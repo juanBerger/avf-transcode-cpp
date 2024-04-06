0133f4a30d99e7acdc9921965035b3a9e12180e8
juanBerger <jaboit@gmail.com>
2024-04-06 10:25:02 -0400

    added transcode to buffer

--- src/main.mm
+++ src/main.mm
@@ -1,99 +1,86 @@
 #include <iostream>
 #include <AVFoundation/AVFoundation.h>
-
-//void transcodeVideoRange(const std::string& inputFilePath, const std::string& outputFilePath, double startTime, double duration) {
-//
-//    NSURL* inputURL = [NSURL fileURLWithPath:[NSString stringWithUTF8String:inputFilePath.c_str()]];
-//    NSURL* outputURL = [NSURL fileURLWithPath:[NSString stringWithUTF8String:outputFilePath.c_str()]];
-//
-//    AVURLAsset* asset = [AVURLAsset URLAssetWithURL:inputURL options:nil];
-//    AVAssetExportSession* exportSession = [AVAssetExportSession exportSessionWithAsset:asset presetName:AVAssetExportPresetHighestQuality];
-//
-//    CMTime startTimeValue = CMTimeMakeWithSeconds(startTime, 1000);
-//    CMTime durationValue = CMTimeMakeWithSeconds(duration, 1000);
-//    CMTimeRange timeRange = CMTimeRangeMake(startTimeValue, durationValue);
-//
-//    exportSession.outputURL = outputURL;
-//    exportSession.outputFileType = AVFileTypeQuickTimeMovie; // For QuickTime output format
-//    //exportSession.outputFileType = AVFileTypeMPEG4; // For MPEG-4 output format
-//    exportSession.timeRange = timeRange;
-//
-//    [exportSession exportAsynchronouslyWithCompletionHandler:^{
-//        switch (exportSession.status) {
-//            case AVAssetExportSessionStatusCompleted:
-//                // Transcoding completed successfully
-//                break;
-//            case AVAssetExportSessionStatusFailed:
-//                // Transcoding failed
-//                break;
-//            case AVAssetExportSessionStatusCancelled:
-//                // Transcoding was canceled
-//                break;
-//            default:
-//                break;
-//        }
-//    }];
-//}
+#include <CoreVideo/CoreVideo.h>
 
 void transcode(const std::string& inputPath){
 
     NSURL* inputURL = [NSURL fileURLWithPath:[NSString stringWithUTF8String:inputPath.c_str()]];
-    NSError* error;
+    NSError* error = Nil;
 
-    // Create an AVAsset with a URL pointing at a local asset
-    AVAsset *sourceMovieAsset = [AVAsset assetWithURL:inputURL];
-
-    // Create an AVAssetReader for the asset
-    AVAssetReader *assetReader = [AVAssetReader assetReaderWithAsset:sourceMovieAsset
+    // Create an AVAsset with a URL pointing at a local asset. Create an AVAssetReader for the asset
+    AVAsset *srcAsset = [AVAsset assetWithURL:inputURL];
+    AVAssetReader *srcAssetReader = [AVAssetReader assetReaderWithAsset:srcAsset
                                                                error:&error];
 
     // Copy the array of video tracks from the source movie
-    NSArray<AVAssetTrack*>  *tracks = [sourceMovieAsset tracksWithMediaType:AVMediaTypeVideo];
+    NSArray<AVAssetTrack*>  *tracks = [srcAsset tracksWithMediaType:AVMediaTypeVideo];
+    AVAssetTrack *track = [tracks objectAtIndex:0]; //just gets the first track, will have to modify this
 
-    // Get the first video track
-    AVAssetTrack *track = [tracks objectAtIndex:0];
-
-    // Create the asset reader track output for this video track, requesting ‘y416’ output
+    //Create the asset reader track output for this video track, requesting ‘y416’ output.
+    //Not sure what to set here,
     NSDictionary *outputSettings = @{(id)kCVPixelBufferPixelFormatTypeKey :
                                       @(kCVPixelFormatType_4444AYpCbCr16) };
 
     AVAssetReaderTrackOutput* assetReaderTrackOutput
-            = [AVAssetReaderTrackOutput assetReaderTrackOutputWithTrack:track
-                                                         outputSettings:outputSettings];
+            = [AVAssetReaderTrackOutput assetReaderTrackOutputWithTrack:track outputSettings:outputSettings];
 
-    // Set the property to instruct the track output to return the samples
-    // without copying them
-    assetReaderTrackOutput.alwaysCopiesSampleData = NO;
+    assetReaderTrackOutput.alwaysCopiesSampleData = NO; //this is important
+    [srcAssetReader addOutput:assetReaderTrackOutput]; //Connect the AVAssetReaderTrackOutput to the AVAssetReader
 
-    // Connect the AVAssetReaderTrackOutput to the AVAssetReader
-    [assetReader addOutput:assetReaderTrackOutput];
+    //Set up the writer
+    NSDictionary *writerSettings = @{
+            AVVideoCodecKey: AVVideoCodecTypeH264,
+            AVVideoWidthKey: @(960),
+            AVVideoHeightKey: @(540),
+            AVVideoCompressionPropertiesKey: @{
+                    AVVideoAverageBitRateKey: @(400000)
+            }
+    };
+    AVAssetWriterInput *writerInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo outputSettings:writerSettings];
+    writerInput.expectsMediaDataInRealTime = YES;
+    AVAssetWriterInputPixelBufferAdaptor *adaptor = [AVAssetWriterInputPixelBufferAdaptor assetWriterInputPixelBufferAdaptorWithAssetWriterInput:writerInput sourcePixelBufferAttributes:nil];
 
-    BOOL success = [assetReader startReading];
-    std::cout << success << std::endl;
+    BOOL success = [srcAssetReader startReading];
 
-//    if (success) {
-//        CMSampleBufferRef sampleBuffer = NULL;
-//
-//        // output is a AVAssetReaderOutput
-//        while ((sampleBuffer = [output copyNextSampleBuffer]))
-//        {
-//            CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
-//
-//            if (imageBuffer)
-//            {
-//
-//                // Use the image buffer here
-//                // if imageBuffer is NULL, this is likely a marker sampleBuffer
-//            }
-//        }
-//    }
+    if (success) {
 
+        CMSampleBufferRef sampleBuffer = NULL;
+        // output is a AVAssetReaderOutput
+        while ((sampleBuffer = [assetReaderTrackOutput copyNextSampleBuffer]))
+        {
+            CVImageBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+
+            if (pixelBuffer){
+
+                if ([writerInput isReadyForMoreMediaData]) {
+                    [adaptor appendPixelBuffer:pixelBuffer withPresentationTime:CMSampleBufferGetPresentationTimeStamp(sampleBuffer)];
+                }
+
+                CVPixelBufferLockBaseAddress(pixelBuffer, 0);
+                void *pixelData = CVPixelBufferGetBaseAddress(pixelBuffer);
+                size_t bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer);
+                size_t height = CVPixelBufferGetHeight(pixelBuffer);
+
+                // Create a data object with the pixel data
+                NSData *frameData = [NSData dataWithBytes:pixelData length:bytesPerRow * height];
+
+                // Send the frame data across the network
+                // ...
+
+                CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+
+                // Release the sample buffer
+                CFRelease(sampleBuffer);
+                std::cout << "transcoded frame" << std::endl;
+            }
+        }
+    }
 }
 
 
 int main() {
     
-    std::string inputFile = "./videos/small_prores.mov";
+    std::string inputFile = "/Users/juanaboites/dev/postlink/avf-transcode-cpp/src/videos/large_prores.mov";
     std::string outputFile = "./videos/smp.mov";
     double startTime = 1.0; // Start time in seconds
     double duration = 1.0; // Duration in seconds
